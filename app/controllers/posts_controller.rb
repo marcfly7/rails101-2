eae4853f29db366412c87f4c991b43ad2f20ed25
daijianlin <daijianlin@163.com>
2016-12-18 15:22:37 +0800

    finished the homework regarding the edit post and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,22 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
 
   private
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   end
   namespace :account do
     resources :groups
+    resources :posts
   end
+  resources :posts
   root "groups#index"
   # root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
